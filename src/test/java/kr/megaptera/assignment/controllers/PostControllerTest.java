0a9371e18111f0c46f8b05fccf0c2237e6793d37
Dong Seok Lee <dongseok.lee.log@gmail.com>
2023-03-12 14:17:19 +0900

    ING: test 코드 추가 완료.

--- src/test/java/kr/megaptera/assignment/controllers/PostControllerTest.java
+++ src/test/java/kr/megaptera/assignment/controllers/PostControllerTest.java
@@ -1,34 +1,34 @@
 package kr.megaptera.assignment.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.megaptera.assignment.application.CreatePostService;
+import kr.megaptera.assignment.application.DeletePostService;
+import kr.megaptera.assignment.application.GetPostService;
+import kr.megaptera.assignment.application.GetPostsService;
+import kr.megaptera.assignment.application.UpdatePostService;
 import kr.megaptera.assignment.dtos.PostCreateRequestDto;
 import kr.megaptera.assignment.dtos.PostResponseDto;
 import kr.megaptera.assignment.dtos.PostUpdateRequestDto;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-import static kr.megaptera.assignment.utils.AppStringUtils.PATH_SEPERATOR;
-import static kr.megaptera.assignment.utils.AppStringUtils.POST_REQ_MAP;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasKey;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -38,129 +38,115 @@ class PostControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
-  @Autowired
-  private ObjectMapper objectMapper;
+  @MockBean
+  private GetPostsService getPostsService;
 
-  private List<PostCreateRequestDto> postCreateRequestDtos = new ArrayList<>();
-  private List<PostResponseDto> postResponseDtos = new ArrayList<>();
-  private String titleSamplePrefix = "제목입니다";
-  private String authorSamplePrefix = "글쓴이입니다";
-  private String contentSamplePrefix = "내용입니다";
+  @MockBean
+  private GetPostService getPostService;
 
-  @BeforeEach
-  public void setUp() throws Exception {
-    postCreateRequestDtos = IntStream.range(1, SETUP_ITEM_NUM + 1).mapToObj((number) -> {
-      PostCreateRequestDto postCreateRequestDto =
-        new PostCreateRequestDto(titleSamplePrefix + number,
-          authorSamplePrefix + number,
-          contentSamplePrefix + number);
-      try {
-        MvcResult result = this.mockMvc.perform(
-                                 post("/posts").contentType(MediaType.APPLICATION_JSON)
-                                               .content(objectMapper.writeValueAsString(postCreateRequestDto)))
-                                       .andReturn();
-        PostResponseDto postResponseDto =
-          objectMapper.readValue(result.getResponse().getContentAsString(), PostResponseDto.class);
-        postResponseDtos.add(postResponseDto);
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
-      return postCreateRequestDto;
-    }).collect(Collectors.toList());
-  }
+  @MockBean
+  private CreatePostService createPostService;
+
+  @MockBean
+  private UpdatePostService updatePostService;
+
+  @MockBean
+  private DeletePostService deletePostService;
+
 
   @Test
   @DisplayName("GET /posts")
-    // 게시글 목록을 성공적으로 가져온다.
   void list() throws Exception {
-    //given
+    given(getPostsService.getPosts()).willReturn(List.of(
+      new PostResponseDto("001", "제목1", "작성자1", "내용1"),
+      new PostResponseDto("002", "제목2", "작성자2", "내용2")
+    ));
 
-
-    //when then
-    this.mockMvc.perform(get("/posts")).andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$", hasSize(SETUP_ITEM_NUM)))
-                // 첫번째 아이템이 title이라는 키를 포함하나?
-                .andExpect(jsonPath("$[0]", hasKey("title")));
+    mockMvc.perform(get("/posts"))
+           .andExpect(status().isOk())
+           .andExpect(content().string(
+             containsString("제목1")
+           ));
   }
 
   @Test
   @DisplayName("GET /posts/{id} - with correct ID")
   void detailWithCorrectId() throws Exception {
     // given
+    String id = "123";
 
-    // when
-    this.mockMvc.perform(get("/posts/" + postResponseDtos.get(0).getId()))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasKey("title")));
+    given(getPostService.getPostDatail(id))
+      .willReturn(new PostResponseDto(id, "제목", "작성자", "내용"));
 
-    // then
-
+    mockMvc.perform(get("/posts/" + id))
+           .andExpect(status().isOk())
+           .andExpect(content().string(
+             containsString("제목")
+           ));
   }
 
   @Test
   @DisplayName("GET /posts/{id} - with incorrect ID")
   void detail() throws Exception {
-    // given
+    String id = "999";
 
-    // when
-    this.mockMvc.perform(get(String.format("%s%s%d", POST_REQ_MAP, PATH_SEPERATOR, -1)))
-                .andExpect(status().is4xxClientError());
+    given(getPostService.getPostDatail(id))
+      .willThrow(new PostNotFound());
 
-    // then
+    mockMvc.perform(get("/posts/" + id))
+           .andExpect(status().isNotFound());
   }
 
   @Test
   @DisplayName("POST /posts")
   void create() throws Exception {
-    String title = "제목ㅋ";
-    String author = "글쓴이ㅋ";
-    String content = "내용ㅋ";
-    PostCreateRequestDto postCreateRequestDto = new PostCreateRequestDto(title, author, content);
-    this.mockMvc.perform(
-          post(POST_REQ_MAP).contentType(MediaType.APPLICATION_JSON)
-                            .content(objectMapper.writeValueAsString(postCreateRequestDto)))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$", hasKey("title")))
-                .andExpect(jsonPath("$.title", equalTo(title)))
-                .andExpect(jsonPath("$.author", equalTo(author)))
-                .andExpect(jsonPath("$.content", equalTo(content)));
+    String json = """
+      {
+        "title": "데브로드",
+        "author": "홀맨",
+        "content": "열심히 합시다"
+      }
+      """;
+
+    mockMvc.perform(post("/posts")
+             .contentType(MediaType.APPLICATION_JSON)
+             .content(json)
+           )
+           .andExpect(status().isCreated());
+
+    verify(createPostService).createPost(any(PostCreateRequestDto.class));
   }
 
   @Test
   @DisplayName("PATCH /posts/{id}")
   void update() throws Exception {
-    String title = "업데이트 제목";
-    String content = "업데이트된 내용";
-    PostUpdateRequestDto postUpdateRequestDto = new PostUpdateRequestDto(title, content);
-    final int UPDATE_TARGET_IDX = 0;
-    final PostResponseDto targetPostResponseDto = postResponseDtos.get(UPDATE_TARGET_IDX);
-    this.mockMvc.perform(
-          patch(POST_REQ_MAP + PATH_SEPERATOR + targetPostResponseDto.getId()).contentType(
-                                                                                MediaType.APPLICATION_JSON)
-                                                                              .content(
-                                                                                objectMapper.writeValueAsString(
-                                                                                  postUpdateRequestDto)))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$", hasKey("title")))
-                .andExpect(jsonPath("$.title", equalTo(title)))
-                .andExpect(jsonPath("$.content", equalTo(content)))
-                .andExpect(jsonPath("$.id", equalTo(targetPostResponseDto.getId())))
-                .andExpect(jsonPath("$.author", equalTo(targetPostResponseDto.getAuthor())));
+    String id = "001POST";
+
+    String json = """
+      {
+        "title": "데브로드",
+        "content": "열심히 합시다"
+      }
+      """;
+
+    mockMvc.perform(patch("/posts/" + id)
+             .contentType(MediaType.APPLICATION_JSON)
+             .content(json)
+           )
+           .andExpect(status().isOk());
+
+    verify(updatePostService)
+      .updatePost(eq(id), any(PostUpdateRequestDto.class));
   }
 
   @Test
   @DisplayName("DELETE /posts/{id}")
   void deletePost() throws Exception {
-    final int DELETE_TARGET_IDX = 0;
-    final PostResponseDto targetPostResponseDto = postResponseDtos.get(DELETE_TARGET_IDX);
-    this.mockMvc.perform(
-          patch(POST_REQ_MAP + PATH_SEPERATOR + targetPostResponseDto.getId()))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasKey("title")))
-                .andExpect(jsonPath("$.title", equalTo(targetPostResponseDto.getTitle())))
-                .andExpect(jsonPath("$.content", equalTo(targetPostResponseDto.getContent())))
-                .andExpect(jsonPath("$.id", equalTo(targetPostResponseDto.getId())))
-                .andExpect(jsonPath("$.author", equalTo(targetPostResponseDto.getAuthor())));
+    String id = "001POST";
+
+    mockMvc.perform(delete("/posts/" + id))
+           .andExpect(status().isOk());
+
+    verify(deletePostService).deletePost(id);
   }
 }
