9e93ca5e3865f5ca5aaf8f8248288547402a302c
spacekay <bethjkim0311@gmail.com>
2023-05-14 23:40:07 +0900

    feat: Comment 관련 Model objects

---
+++ src/main/java/kr/megaptera/assignment/domains/model/Comment.java
@@ -0,0 +1,42 @@
+package kr.megaptera.assignment.domains.model;
+
+import kr.megaptera.assignment.domains.dto.CommentCreateDto;
+import kr.megaptera.assignment.domains.dto.CommentUpdateDto;
+
+public class Comment {
+
+    private CommentId id;
+
+    private PostId postId;
+
+    private CommentAuthor author;
+
+    private MultilineText content;
+
+    public Comment(PostId postId, CommentCreateDto commentCreateDto) {
+        this.id = new CommentId();
+        this.postId = postId;
+        this.author = CommentAuthor.of(commentCreateDto.getAuthor());
+        this.content = MultilineText.of(commentCreateDto.getContent());
+    }
+
+    public CommentId id() {
+        return this.id;
+    }
+
+    public PostId postId() {
+        return this.postId;
+    }
+
+    public CommentAuthor author() {
+        return this.author;
+    }
+
+    public MultilineText content() {
+        return this.content;
+    }
+
+    public void updateComment(CommentUpdateDto commentUpdateDto) {
+        this.content = MultilineText.of(commentUpdateDto.getContent());
+    }
+}
