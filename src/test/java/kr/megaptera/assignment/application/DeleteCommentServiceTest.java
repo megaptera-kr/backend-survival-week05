ca92c111e137633f93e2e2a6f1b401e04d01352f
spacekay <bethjkim0311@gmail.com>
2023-05-15 01:12:53 +0900

    feat: Comment 기능 테스트 코드 작성

--- src/test/java/kr/megaptera/assignment/application/DeleteCommentServiceTest.java
+++ src/test/java/kr/megaptera/assignment/application/DeleteCommentServiceTest.java
@@ -1,4 +1,56 @@
 package kr.megaptera.assignment.application;
 
+import kr.megaptera.assignment.domains.dto.CommentDto;
+import kr.megaptera.assignment.domains.model.Comment;
+import kr.megaptera.assignment.domains.model.CommentAuthor;
+import kr.megaptera.assignment.domains.model.CommentId;
+import kr.megaptera.assignment.domains.model.MultilineText;
+import kr.megaptera.assignment.domains.model.PostId;
+import kr.megaptera.assignment.exceptions.CommentNotFound;
+import kr.megaptera.assignment.repositories.CommentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 class DeleteCommentServiceTest {
+
+    private CommentRepository commentRepository;
+
+    private DeleteCommentService deleteCommentService;
+
+    @BeforeEach
+    void setUp() {
+        commentRepository = mock(CommentRepository.class);
+        deleteCommentService = new DeleteCommentService(commentRepository);
+    }
+
+    @Test
+    @DisplayName("게시물에 이미 존재하는 댓글 삭제")
+    void delete() {
+        given(commentRepository.find(CommentId.of("3"), PostId.of("1")))
+                .willReturn(
+                        new Comment(
+                                CommentId.of("3"),
+                                PostId.of("1"),
+                                CommentAuthor.of("케이"),
+                                MultilineText.of("감사합니다!\n운영자님")
+                        )
+                );
+
+        CommentDto commentDto = deleteCommentService.deleteComment("3", "1");
+
+        verify(commentRepository).delete(any(CommentId.class), any(PostId.class));
+
+        assertThat(commentDto.getContent()).contains("운영자님");
+        assertThrows(CommentNotFound.class, () -> deleteCommentService.deleteComment("4", "1"));
+        assertThrows(CommentNotFound.class, () -> deleteCommentService.deleteComment("3", "10"));
+    }
+
 }
--- src/test/java/kr/megaptera/assignment/application/UpdateCommentServiceTest.java
+++ src/test/java/kr/megaptera/assignment/application/UpdateCommentServiceTest.java
@@ -1,4 +1,57 @@
 package kr.megaptera.assignment.application;
 
+import kr.megaptera.assignment.domains.dto.CommentDto;
+import kr.megaptera.assignment.domains.dto.CommentUpdateDto;
+import kr.megaptera.assignment.domains.model.Comment;
+import kr.megaptera.assignment.domains.model.CommentAuthor;
+import kr.megaptera.assignment.domains.model.CommentId;
+import kr.megaptera.assignment.domains.model.MultilineText;
+import kr.megaptera.assignment.domains.model.PostId;
+import kr.megaptera.assignment.exceptions.CommentNotFound;
+import kr.megaptera.assignment.repositories.CommentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 class UpdateCommentServiceTest {
+
+    private CommentRepository commentRepository;
+
+    private UpdateCommentService updateCommentService;
+
+    @BeforeEach
+    void setUp() {
+        commentRepository = mock(CommentRepository.class);
+        updateCommentService = new UpdateCommentService(commentRepository);
+    }
+
+    @Test
+    @DisplayName("게시물에 이미 존재하는 댓글 수정")
+    void updateComment() {
+        given(commentRepository.find(CommentId.of("3"), PostId.of("1")))
+                .willReturn(
+                        new Comment(
+                                CommentId.of("3"),
+                                PostId.of("1"),
+                                CommentAuthor.of("케이"),
+                                MultilineText.of("감사합니다!\n운영자님")
+                        )
+                );
+
+        CommentUpdateDto commentUpdateDto = new CommentUpdateDto("아차, 수정하겠습니다.\n또 만나요~");
+        CommentDto commentDto = updateCommentService.updateComment("3", "1", commentUpdateDto);
+
+        verify(commentRepository).save(any(PostId.class), any(Comment.class));
+        assertThat(commentDto.getContent()).contains("수정하겠습니다");
+        assertThrows(CommentNotFound.class, () -> updateCommentService.updateComment("4", "1", commentUpdateDto));
+        assertThrows(CommentNotFound.class, () -> updateCommentService.updateComment("3", "10", commentUpdateDto));
+    }
+
 }
