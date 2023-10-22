6d9640fc1b7f80f1108c253dbe89574d9b8b8992
hoeseong <hoeseong19@naver.com>
2023-10-22 17:31:53 +0900

    게시물 관련 서비스 구현

--- src/main/java/kr/megaptera/assignment/application/posts/CreatePostService.java
+++ src/main/java/kr/megaptera/assignment/application/posts/CreatePostService.java
@@ -1,4 +1,24 @@
 package kr.megaptera.assignment.application.posts;
 
+import kr.megaptera.assignment.dtos.posts.CreatePostDto;
+import kr.megaptera.assignment.dtos.posts.PostDto;
+import kr.megaptera.assignment.models.posts.Post;
+import kr.megaptera.assignment.repositories.PostRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CreatePostService {
+    private final PostRepository postRepository;
+
+    public CreatePostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public PostDto create(CreatePostDto createPostDto) {
+        Post newPost = Post.of(createPostDto);
+
+        this.postRepository.save(newPost);
+
+        return PostDto.of(newPost);
+    }
 }
