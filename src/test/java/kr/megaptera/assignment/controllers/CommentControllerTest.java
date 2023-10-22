644e1fd0c218ef44e01a1cfc5ade5a77452de334
hoeseong <hoeseong19@naver.com>
2023-10-22 21:20:45 +0900

    ControllerTest 작성

--- src/test/java/kr/megaptera/assignment/controllers/CommentControllerTest.java
+++ src/test/java/kr/megaptera/assignment/controllers/CommentControllerTest.java
@@ -1,5 +1,105 @@
 package kr.megaptera.assignment.controllers;
 
+import kr.megaptera.assignment.application.comments.CreateCommentService;
+import kr.megaptera.assignment.application.comments.DeleteCommentService;
+import kr.megaptera.assignment.application.comments.GetCommentsService;
+import kr.megaptera.assignment.application.comments.UpdateCommentService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(CommentController.class)
 class CommentControllerTest {
-    // TODO: PostControllerTest 를 참고해서 테스트를 작성해 주세요.
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CreateCommentService createCommentService;
+    @MockBean
+    private DeleteCommentService deleteCommentService;
+    @MockBean
+    private GetCommentsService getCommentsService;
+    @MockBean
+    private UpdateCommentService updateCommentService;
+
+    @Test
+    @DisplayName("GET /comments")
+    void list() throws Exception {
+        String mockPostId = "1";
+
+        this.mockMvc.perform(get("/comments?postId=%s".formatted(mockPostId)))
+                .andExpect(status().isOk());
+
+        verify(getCommentsService).findAll(any());
+    }
+
+    @Test
+    @DisplayName("POST /comments")
+    void create() throws Exception {
+        String mockPostId = "1";
+
+        String createCommentJson = """
+                    {
+                        "title":"title",
+                        "author":"author",
+                        "content":"content"
+                    }
+                """;
+
+        RequestBuilder requestBuilder = post("/comments?postId=%s".formatted(mockPostId))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createCommentJson);
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().isCreated());
+
+        verify(createCommentService).create(any(), any());
+    }
+
+    @Test
+    @DisplayName("PATCH /comments/{id}")
+    void update() throws Exception {
+        String mockPostId = "1";
+        String mockCommentId = "1";
+
+        String updateCommentJson = """
+                    {
+                        "title":"title",
+                        "content":"content"
+                    }
+                """;
+
+        RequestBuilder requestBuilder = patch("/comments/%s?postId=%s".formatted(mockCommentId, mockPostId))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(updateCommentJson);
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk());
+
+        verify(updateCommentService).update(any(), any(), any());
+    }
+
+    @Test
+    @DisplayName("DELETE /comments/{id}")
+    void deleteComment() throws Exception {
+        String mockPostId = "1";
+        String mockCommentId = "1";
+
+        RequestBuilder requestBuilder = delete("/comments/%s?postId=%s".formatted(mockCommentId, mockPostId));
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk());
+
+        verify(deleteCommentService).delete(any(), any());
+    }
 }
--- src/test/java/kr/megaptera/assignment/controllers/PostControllerTest.java
+++ src/test/java/kr/megaptera/assignment/controllers/PostControllerTest.java
@@ -1,36 +1,127 @@
 package kr.megaptera.assignment.controllers;
 
+import kr.megaptera.assignment.application.posts.*;
+import kr.megaptera.assignment.dtos.posts.PostDto;
+import kr.megaptera.assignment.exceptions.posts.PostNotFound;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PostController.class)
 class PostControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CreatePostService createPostService;
+    @MockBean
+    private DeletePostService deletePostService;
+    @MockBean
+    private GetPostService getPostService;
+    @MockBean
+    private GetPostsService getPostsService;
+    @MockBean
+    private UpdatePostService updatePostService;
+
     @Test
     @DisplayName("GET /posts")
-    void list() {
+    void list() throws Exception {
+        this.mockMvc.perform(get("/posts"))
+                .andExpect(status().isOk());
+
+        verify(getPostsService).findAll();
     }
 
     @Test
     @DisplayName("GET /posts/{id} - with correct ID")
-    void detailWithCorrectId() {
+    void detailWithCorrectId() throws Exception {
+        String mockPostId = "1";
+
+        given(getPostService.find(any())).willReturn(any(PostDto.class));
+
+        this.mockMvc.perform(get("/posts/%s".formatted(mockPostId)))
+                .andExpect(status().isOk());
+
+        verify(getPostService).find(any());
     }
 
     @Test
     @DisplayName("GET /posts/{id} - with incorrect ID")
-    void detail() {
+    void detail() throws Exception {
+        String mockPostId = "1";
+
+        given(getPostService.find(any())).willThrow(new PostNotFound());
+
+        this.mockMvc.perform(get("/posts/%s".formatted(mockPostId)))
+                .andExpect(status().isNotFound());
+
+        verify(getPostService).find(any());
     }
 
     @Test
     @DisplayName("POST /posts")
-    void create() {
+    void create() throws Exception {
+        String createPostJson = """
+                    {
+                        "title":"title",
+                        "author":"author",
+                        "content":"content"
+                    }
+                """;
+
+        RequestBuilder requestBuilder = post("/posts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(createPostJson);
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().isCreated());
+
+        verify(createPostService).create(any());
     }
 
     @Test
     @DisplayName("PATCH /posts/{id}")
-    void update() {
+    void update() throws Exception {
+        String mockPostId = "1";
+
+        String updatePostJson = """
+                    {
+                        "title":"title",
+                        "content":"content"
+                    }
+                """;
+
+        RequestBuilder requestBuilder = patch("/posts/%s".formatted(mockPostId))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(updatePostJson);
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk());
+
+        verify(updatePostService).update(any(), any());
     }
 
     @Test
     @DisplayName("DELETE /posts/{id}")
-    void deletePost() {
+    void deletePost() throws Exception {
+        String mockPostId = "1";
+
+        RequestBuilder requestBuilder = delete("/posts/%s".formatted(mockPostId));
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk());
+
+        verify(deletePostService).delete(any());
     }
 }
