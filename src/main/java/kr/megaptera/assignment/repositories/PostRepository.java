c81487c8990d0da53cf1e91c7cfe4f0fcb00ef4e
DongJae <tlsehdwo7@naver.com>
2023-10-24 08:50:14 +0900

    add : week5 assignment Test

---
+++ src/main/java/kr/megaptera/assignment/repositories/PostRepository.java
@@ -0,0 +1,36 @@
+package kr.megaptera.assignment.repositories;
+
+import kr.megaptera.assignment.models.Post;
+import kr.megaptera.assignment.models.PostId;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class PostRepository {
+    Map<PostId, Post> posts;
+
+    public PostRepository() {
+        this.posts = new HashMap<>();
+    }
+
+    public List<Post> findAll() {
+        return new ArrayList<>(posts.values());
+    }
+
+    public Post find(PostId id) {
+        Post post = posts.get(id);
+        return post;
+    }
+
+    public void save(Post post) {
+        posts.put(post.id(), post);
+    }
+
+    public void delete(PostId id) {
+        posts.remove(id);
+    }
+}
--- src/test/java/kr/megaptera/assignment/application/CreatePostServiceTest.java
+++ src/test/java/kr/megaptera/assignment/application/CreatePostServiceTest.java
@@ -1,11 +1,41 @@
 package kr.megaptera.assignment.application;
 
+import kr.megaptera.assignment.dtos.PostCreateDto;
+import kr.megaptera.assignment.dtos.PostDto;
+import kr.megaptera.assignment.models.Post;
+import kr.megaptera.assignment.repositories.PostRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 class CreatePostServiceTest {
+    private PostRepository postRepository;
+
+    private CreatePostService createPostService;
+
+    @BeforeEach
+    void setUp() {
+        postRepository = mock(PostRepository.class);
+
+        createPostService = new CreatePostService(postRepository);
+    }
+
     @Test
     @DisplayName("게시물 생성")
     void create() {
+        PostCreateDto newPost = new PostCreateDto("제목", "작성자", "내용");
+
+        PostDto postDto = createPostService.createPost(newPost);
+
+        assertThat(postDto.getTitle()).isEqualTo("제목");
+        assertThat(postDto.getAuthor()).isEqualTo("작성자");
+        assertThat(postDto.getContent()).isEqualTo("내용");
+
+        verify(postRepository).save(any(Post.class));
     }
 }
