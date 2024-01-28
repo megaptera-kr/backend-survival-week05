dce688b4919707e654dbfaba260a9d9855b3f6fb
LIG-JY <dldlsry053@naver.com>
2024-01-28 23:59:13 +0900

    Test : 어플리케이션 레이어 delete post, delete comment 테스트 코드 작성

--- src/test/java/kr/megaptera/assignment/application/DeleteCommentServiceTest.java
+++ src/test/java/kr/megaptera/assignment/application/DeleteCommentServiceTest.java
@@ -1,4 +1,83 @@
 package kr.megaptera.assignment.application;
 
+import kr.megaptera.assignment.Dtos.CommentDto;
+import kr.megaptera.assignment.exceptions.CommentNotFoundException;
+import kr.megaptera.assignment.exceptions.PostNotFoundException;
+import kr.megaptera.assignment.models.Comment;
+import kr.megaptera.assignment.models.CommentId;
+import kr.megaptera.assignment.models.MultilineText;
+import kr.megaptera.assignment.models.Post;
+import kr.megaptera.assignment.models.PostId;
+import kr.megaptera.assignment.repositories.CommentRepository;
+import kr.megaptera.assignment.repositories.PostRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 class DeleteCommentServiceTest {
+    private CommentRepository commentRepository;
+    private PostRepository postRepository;
+    private DeleteCommentService deleteCommentService;
+
+    @BeforeEach
+    void setUp() {
+        commentRepository = mock(CommentRepository.class);
+        postRepository = mock(PostRepository.class);
+        deleteCommentService = new DeleteCommentService(commentRepository, postRepository);
+    }
+
+    @Test
+    @DisplayName("Delete Existing Comment")
+    void deleteExistingComment() {
+        // Arrange
+        Post post = new Post("title", "author", MultilineText.from("content"));
+        PostId postId = post.id();
+        Comment comment = new Comment("author", MultilineText.from("content"));
+        CommentId commentId = comment.id();
+        when(postRepository.findById(postId)).thenReturn(post);
+        when(commentRepository.findAll(postId)).thenReturn(Collections.singletonList(comment));
+
+        // Act
+        CommentDto outputCommentDto = deleteCommentService.delete(commentId.toString(), postId.toString());
+
+        // Assert
+        verify(postRepository).findById(postId);
+        verify(commentRepository).findAll(postId);
+        verify(commentRepository).delete(postId, commentId);
+        assertThat(outputCommentDto).isNotNull();
+        assertThat(outputCommentDto.getAuthor()).isEqualTo(comment.author());
+        assertThat(outputCommentDto.getContent()).isEqualTo(comment.content().toString());
+    }
+
+    @Test
+    @DisplayName("Delete Comment When Post Not Exists")
+    void deleteNonExistingComment() {
+        // Arrange
+        PostId postId = PostId.from("non-exist post id");
+        when(postRepository.findById(postId)).thenReturn(null);
+
+        // Act
+        assertThrows(PostNotFoundException.class, () -> deleteCommentService.delete("any comment Id", postId.toString()));
+    }
+
+    @Test
+    @DisplayName("Delete Comment When Comment Not Exists")
+    void deleteNonExistingComment2() {
+        // Arrange
+        Post post = new Post("title", "author", MultilineText.from("content"));
+        PostId postId = post.id();
+        when(postRepository.findById(postId)).thenReturn(post);
+        when(commentRepository.findAll(postId)).thenReturn(Collections.emptyList());
+
+        // Act
+        assertThrows(CommentNotFoundException.class, () -> deleteCommentService.delete("non existing comment id", postId.toString()));
+    }
 }
--- src/test/java/kr/megaptera/assignment/application/DeletePostServiceTest.java
+++ src/test/java/kr/megaptera/assignment/application/DeletePostServiceTest.java
@@ -1,11 +1,59 @@
 package kr.megaptera.assignment.application;
 
+import kr.megaptera.assignment.Dtos.PostDto;
+import kr.megaptera.assignment.exceptions.PostNotFoundException;
+import kr.megaptera.assignment.models.MultilineText;
+import kr.megaptera.assignment.models.Post;
+import kr.megaptera.assignment.models.PostId;
+import kr.megaptera.assignment.repositories.PostRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 class DeletePostServiceTest {
+
+    private PostRepository postRepository;
+    private DeletePostService deletePostService;
+
+    @BeforeEach
+    void setUp() {
+        postRepository = mock(PostRepository.class);
+        deletePostService = new DeletePostService(postRepository);
+    }
+
     @Test
-    @DisplayName("게시물 삭제")
-    void delete() {
+    @DisplayName("Delete Existing Post")
+    void deleteExistingPost() {
+        // Arrange
+        Post post = new Post("title", "author", MultilineText.from("content"));
+        when(postRepository.findById(post.id())).thenReturn(post);
+
+        // Act
+        PostDto outputPostDto = deletePostService.delete(post.id().toString());
+
+        // Assert
+        verify(postRepository).findById(post.id());
+        verify(postRepository).delete(post.id());
+        assertThat(outputPostDto).isNotNull();
+        assertThat(outputPostDto.getTitle()).isEqualTo(post.title());
+        assertThat(outputPostDto.getAuthor()).isEqualTo(post.author());
+        assertThat(outputPostDto.getContent()).isEqualTo(post.content().toString());
+    }
+
+    @Test
+    @DisplayName("Delete Non-Existing Post")
+    void deleteNonExistingPost() {
+        //Arrange
+        String postId = "non-existing-post-id";
+        when(postRepository.findById(PostId.from(postId))).thenReturn(null);
+
+        // Act
+        assertThrows(PostNotFoundException.class, () -> deletePostService.delete(postId));
     }
 }
